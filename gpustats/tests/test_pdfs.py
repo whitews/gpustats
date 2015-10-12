c8d61e007eac4f3362d8406bf5c84ab5ad767640
Scott White <whitews@gmail.com>
2015-10-12 16:39:12 -0400

    update test/example scripts, seems everything is working again

--- gpustats/tests/test_pdfs.py
+++ gpustats/tests/test_pdfs.py
@@ -4,7 +4,6 @@ from numpy.random import randn
 from numpy.testing import assert_almost_equal
 import numpy as np
 
-import scipy.stats as sp_stats
 from scipy import linalg
 from pymc.distributions import rwishart
 from pymc import mv_normal_cov_like as pdf_func
@@ -56,20 +55,6 @@ def _compare_multi(n, k, p):
     return result, py_result
 
 
-def _compare_single(n, k):
-    data, means, covs = _make_test_case(n, k, 1)
-
-    mean = means[0]
-    cov = covs[0]
-
-    # cpu in PyMC
-    py_result = python_mvnpdf(data, [mean], [cov]).squeeze()
-    # gpu
-
-    result = gps.mvnpdf(data, mean, cov)
-    return result, py_result
-
-
 class TestMVN(unittest.TestCase):
     # n data, dim, n components
     test_cases = [(1000, 4, 1),
@@ -88,45 +73,9 @@ class TestMVN(unittest.TestCase):
         a, b = _compare_multi(n, k, p)
         assert_almost_equal(a, b, DECIMAL_2)
 
-    def _check_single(self, n, k):
-        a, b = _compare_single(n, k)
-        assert_almost_equal(a, b, DECIMAL_2)
-
     def test_multi(self):
         for n, k, p in self.test_cases:
             self._check_multi(n, k, p)
-
-    def test_single(self):
-        for n, k, p in self.test_cases:
-            self._check_single(n, k)
-
-
-class TestUnivariate(unittest.TestCase):
-    def test_normal(self):
-        test_cases = [
-            (100, 0, 1),
-            (100, .5, 2.5),
-            (10, 5, 3),
-            (2000, 1, 4)
-        ]
-        for n, mean, std in test_cases:
-            data = randn(n)
-            py_result = sp_stats.norm.pdf(data, loc=mean, scale=std)
-
-            result = gps.normpdf(data, mean, std, logged=True)
-            assert_almost_equal(result, np.log(py_result), DECIMAL_5)
-
-    def test_normal_multi(self):
-        means = np.random.randn(5)
-        scales = np.ones(5)
-
-        data = np.random.randn(10)
-        result = gps.normpdf_multi(data, means, scales, logged=True)
-
-        py_result = np.empty_like(result)
-        for i, (m, sc) in enumerate(zip(means, scales)):
-            py_result[:, i] = sp_stats.norm.pdf(data, loc=m, scale=sc)
-        assert_almost_equal(result, np.log(py_result), DECIMAL_5)
 
 
 if __name__ == '__main__':
