7df13d960934b3f0e5b10d93947a26504cd8d314
Scott White <whitews@gmail.com>
2015-10-12 16:35:03 -0400

    re-org Kernel classes in codegen, removing univariate caller & cpustub CUDA files

--- gpustats/codegen.py
+++ gpustats/codegen.py
@@ -55,14 +55,8 @@ def _get_support_code():
 
 
 def _get_mvcaller_code():
-    # for multivariate pdfs
+    # for multivariate PDFs
     path = os.path.join(get_cufiles_path(), 'mvcaller.cu')
-    return open(path).read()
-
-
-def _get_univcaller_code():
-    # For univariate pdfs
-    path = os.path.join(get_cufiles_path(), 'univcaller.cu')
     return open(path).read()
 
 
@@ -79,10 +73,10 @@ class Kernel(object):
         caller = self.get_caller()
         return '\n'.join((logic, caller))
 
-    def get_logic(self, **kwds):
+    def get_logic(self, **kwargs):
         raise NotImplementedError
 
-    def get_caller(self, **kwds):
+    def get_caller(self, **kwargs):
         raise NotImplementedError
 
     def get_name(self, name=None):
@@ -99,10 +93,10 @@ class CUFile(Kernel):
     generation framework. Kernel need only have a template to be able to change
     the name of the generated kernel
     """
-    def __init__(self, name, filepath):
+    def __init__(self, name, file_path):
         self.full_path = os.path.join(
             get_cufiles_path(),
-            filepath
+            file_path
         )
 
         Kernel.__init__(self, name)
@@ -111,33 +105,21 @@ class CUFile(Kernel):
         code = open(self.full_path).read()
         return code % {'name': self.name}
 
+    def get_logic(self, **kwargs):
+        raise NotImplementedError
 
-class SamplerKernel(Kernel):
+    def get_caller(self, **kwargs):
+        raise NotImplementedError
+
+
+class MVDensityKernel(Kernel):
     """
-    Holds info for measure sample kernel.
+    Generate kernel for multi-variate probability density function
     """
+    _caller = _get_mvcaller_code()
+
     def __init__(self, name, logic_code):
         self.logic_code = logic_code
-        Kernel.__init__(self, name)
-
-    def get_logic(self, name=None):
-        return self.logic_code
-
-    def get_caller(self, name=None):
-        return self._caller % {'name': self.get_name(name)}
-
-
-class DensityKernel(Kernel):
-    """
-    Generate kernel for probability density function
-    """
-
-    _caller = _get_univcaller_code()
-
-    def __init__(self, name, logic_code):
-
-        self.logic_code = logic_code
-
         Kernel.__init__(self, name)
 
     def get_logic(self, name=None):
@@ -147,29 +129,12 @@ class DensityKernel(Kernel):
         return self._caller % {'name': self.get_name(name)}
 
 
-class MVDensityKernel(DensityKernel):
-    """
+class Exp(Kernel):
+    op = 'expf'
 
-    """
-    _caller = _get_mvcaller_code()
-
-
-class Transform(Kernel):
-    """
-    Enable simple transforms of kernels to compute modified kernel code stub
-    """
     def __init__(self, name, kernel):
         self.kernel = kernel
         Kernel.__init__(self, name)
-
-    # XXX: HACK, not general for non-density kernels
-    def is_multivariate(self):
-        return isinstance(self.kernel, MVDensityKernel)
-
-
-# flop the right name?
-class Flop(Transform):
-    op = None
 
     def get_logic(self, name=None):
         name = self.get_name(name)
@@ -177,10 +142,11 @@ class Flop(Transform):
         actual_name = '%s_stub' % name
         kernel_logic = self.kernel.get_logic(name=actual_name)
 
-        if self.is_multivariate():
-            stub_caller = _mv_stub_caller
-        else:
-            stub_caller = _univ_stub_caller
+        stub_caller = """
+__device__ float %(name)s(float* x, float* params, int dim) {
+    return %(op)s(%(actual_kernel)s(x, params, dim));
+}
+"""
 
         transform_logic = stub_caller % {
             'name': name,
@@ -193,30 +159,6 @@ class Flop(Transform):
     def get_caller(self):
         return self.kernel.get_caller(self.name)
 
-_univ_stub_caller = """
-__device__ float %(name)s(float* x, float* params) {
-    return %(op)s(%(actual_kernel)s(x, params));
-}
-"""
-
-_mv_stub_caller = """
-__device__ float %(name)s(float* x, float* params, int dim) {
-    return %(op)s(%(actual_kernel)s(x, params, dim));
-}
-"""
-
-
-class Exp(Flop):
-    op = 'expf'
-
-
-class Log(Flop):
-    op = 'logf'
-
-
-class Sqrt(Flop):
-    op = 'sqrtf'
-
 _cu_module = None
 
 
@@ -225,8 +167,6 @@ def get_full_cuda_module():
     global _cu_module
 
     if _cu_module is None:
-        objects = kernels.__dict__
-
         all_kernels = dict(
             (k, v)
             for k, v in kernels.__dict__.iteritems()
@@ -235,6 +175,3 @@ def get_full_cuda_module():
         _cu_module = CUDAModule(all_kernels)
 
     return _cu_module
-
-if __name__ == '__main__':
-    pass
